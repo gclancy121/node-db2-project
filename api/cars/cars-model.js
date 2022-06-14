5c2b6deb1888a9b14368ee13f2fe10a1fdbeff64
Gage Clancy <gclancy121@gmail.com>
2022-06-14 15:56:24 -0500

    initial commit

--- api/cars/cars-model.js
+++ api/cars/cars-model.js
@@ -1,11 +1,21 @@
-const getAll = () => {
-  // DO YOUR MAGIC
+const db = require('../../data/db-config');
+
+function getAll() {
+  return db('cars');
 }
 
-const getById = () => {
-  // DO YOUR MAGIC
+function getById(id) {
+  return db('cars').where('id', id).first();
 }
 
-const create = () => {
-  // DO YOUR MAGIC
+async function create(car) {
+  const [id] = await db('cars').insert(car);
+  return getById(id);
+}
+
+module.exports = {
+  getAll,
+  getById,
+  create,
+
 }
--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,1 +1,15 @@
 // DO YOUR MAGIC
+const Cars = require('./cars-model');
+const router = require('express').Router();
+
+router.get('/', (req, res) => {
+  Cars.getAll().then(cars => {
+    res.status(200).json(cars);
+  })
+})
+
+router.use((error, req, res, next) => {
+  res.status(error.status || 500).json({message: error.message, stack: error.stack});
+});
+
+module.exports = router;
